ab4b4b29ae64931dcb1abcf07b1c1685d90e9079
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-22 10:38:46 +0800

    kernel: kp_ksud: restore kprobes for early-boot and used-once hooks
    since kprobes offer dynamic hooking and shit, this is going to be better
    on something that we only need temporarily.
    
    this still keeps whole sucompat onto manual hooks as those are performance
    sensitive, needed to be permanent and "timeable".
    
    as for these hooks that got hooked here they are only used either only at
    boot or on some, used only once.
    
    symbols hooked:
    
    security_bprm_check
    - this thing is only needed at boot, so we might as well put this dynamic
      this is needed for _ksud exec decision making and 32-on-64 detection.
    
    vfs_read
    - needed only at boot for read proxy-ing atrace.rc
    
    input_event
    - needed only up to like boot_complete, for 3-button-press safemode feature
    
    security_key_permission
    - needed for a keygrab on allowlist workaround for kernels below 4.10 and some
    
    sys_execve *
    - a substitute for security_bprm_check. this is deadcoded here, so just for reference
    purposes.
    --
    
    for handling registration of those "LSM-hooks-being kprobed", we register once preempt
    is suspended.
    
    as for unregistration, we defer this once boot is completed and then have a kthread
    unregister everything.
    
    credits: some of these are just straight up copied from upstream.
    
    Tests:
    ximi mi a2 lite, arm64, Linux 4.9
    samsung galaxy s3, arm, Linux 3.0
    https://github.com/backslashxx/KernelSU/issues/26
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

---
+++ kernel/kp_ksud.c
@@ -0,0 +1,207 @@
+#include <linux/version.h>
+#include <linux/kprobes.h>
+#include <linux/printk.h>
+#include <linux/types.h>
+#include <linux/uaccess.h>
+#include <linux/binfmts.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+
+static struct task_struct *unregister_thread;
+
+#include "arch.h"
+#include "klog.h"
+#include "ksud.h"
+#include "kernel_compat.h"
+
+#if 0
+static int sys_execve_handler_pre(struct kprobe *p, struct pt_regs *regs)
+{
+	struct pt_regs *real_regs = PT_REAL_REGS(regs);
+	const char __user *filename_user = (const char __user *)PT_REGS_PARM1(real_regs);
+	const char __user *const __user *__argv = (const char __user *const __user *)PT_REGS_PARM2(real_regs);
+
+	char path[32];
+
+	if (!filename_user)
+		return 0;
+
+	if (ksu_copy_from_user_retry(path, filename_user, sizeof(path)))
+		return 0;
+
+	path[sizeof(path) - 1] = '\0';
+
+	// not /system/bin/init, not /init, not /system/bin/app_process (64/32 thingy)
+	// we dont care !!
+	if (likely(strcmp(path, "/system/bin/init") && strcmp(path, "/init")
+		&& !strstarts(path, "/system/bin/app_process") ))
+		return 0;
+
+// argv stage
+	char argv1[32] = {0};
+	// memzero_explicit(argv1, 32);
+	if (__argv) {
+		const char __user *arg1_user = NULL;
+		// grab argv[1] pointer
+		// this looks like
+		/* 
+		 * 0x1000 ./program << this is __argv
+		 * 0x1001 -o 
+		 * 0x1002 arg
+		*/
+		if (ksu_copy_from_user_retry(&arg1_user, __argv + 1, sizeof(arg1_user)))
+			goto submit; // copy argv[1] pointer fail, probably no argv1 !!
+
+		if (arg1_user)
+			ksu_copy_from_user_retry(argv1, arg1_user, sizeof(argv1));
+	}
+
+submit:
+	argv1[sizeof(argv1) - 1] = '\0';
+	// pr_info("%s: filename: %s argv[1]:%s\n", __func__, path, argv1);
+
+	return ksu_handle_bprm_ksud(path, argv1, NULL, NULL);
+}
+
+
+static struct kprobe execve_kp = {
+	.symbol_name = SYS_EXECVE_SYMBOL,
+	.pre_handler = sys_execve_handler_pre,
+};
+#endif
+
+// bprm
+static void kp_stop_bprm_check_hook();
+extern int ksu_bprm_check(struct linux_binprm *bprm);
+
+static int bprm_check_handler_pre(struct kprobe *p, struct pt_regs *regs)
+{
+	struct linux_binprm *bprm_local = (struct linux_binprm *)PT_REGS_PARM1(regs);
+
+	return ksu_bprm_check(bprm_local);
+};
+
+// yes, we're hooking the LSM via kprobe. fite me.
+static struct kprobe bprm_check_kp = {
+	.symbol_name = "security_bprm_check",
+	.pre_handler = bprm_check_handler_pre,
+};
+
+// vfs_read
+static void kp_stop_vfs_read_hook();
+extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
+			size_t *count_ptr, loff_t **pos);
+
+static int vfs_read_handler_pre(struct kprobe *p, struct pt_regs *regs)
+{
+	struct file **file_ptr = (struct file **)&PT_REGS_PARM1(regs);
+	char __user **buf_ptr = (char **)&PT_REGS_PARM2(regs);
+	size_t *count_ptr = (size_t *)&PT_REGS_PARM3(regs);
+	loff_t **pos_ptr = (loff_t **)&PT_REGS_CCALL_PARM4(regs);
+
+	return ksu_handle_vfs_read(file_ptr, buf_ptr, count_ptr, pos_ptr);
+}
+
+static struct kprobe vfs_read_kp = {
+	.symbol_name = "vfs_read",
+	.pre_handler = vfs_read_handler_pre,
+};
+
+// input_event
+static void kp_stop_input_hook();
+extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
+
+static int input_handle_event_handler_pre(struct kprobe *p, struct pt_regs *regs)
+{
+	unsigned int *type = (unsigned int *)&PT_REGS_PARM2(regs);
+	unsigned int *code = (unsigned int *)&PT_REGS_PARM3(regs);
+	int *value = (int *)&PT_REGS_CCALL_PARM4(regs);
+
+	return ksu_handle_input_handle_event(type, code, value);
+
+};
+
+static struct kprobe input_event_kp = {
+	.symbol_name = "input_event",
+	.pre_handler = input_handle_event_handler_pre,
+};
+
+// key_permission
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
+static void kp_stop_key_permission_hook();
+extern int ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
+			      unsigned perm);
+
+static int key_permission_handler_pre(struct kprobe *p, struct pt_regs *regs)
+{
+	key_ref_t key_ref_local = (key_ref_t)PT_REGS_PARM1(regs);
+	const struct cred *cred_local = (const struct cred *)PT_REGS_PARM2(regs);
+	unsigned int perm_local = (unsigned int)PT_REGS_PARM3(regs);
+
+	return ksu_key_permission(key_ref_local, cred_local, perm_local);
+
+};
+
+static struct kprobe key_permission_kp = {
+	.symbol_name = "security_key_permission",
+	.pre_handler = key_permission_handler_pre,
+};
+#endif // key_permission
+
+static void unregister_kprobe_logged(struct kprobe *kp, const char *name)
+{
+	if (!kp->addr) {
+		pr_info("unregister_kprobe: %s not registered in the first place\n");
+		return;
+	}
+
+	unregister_kprobe(kp); // this fucking shit has no return code
+	pr_info("unregister_kprobe: %s ??\n", name);
+}
+
+static int unregister_kprobe_function(void *data)
+{
+	pr_info("kprobe_unregister: unregistering kprobes...\n");
+
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
+	unregister_kprobe_logged(&key_permission_kp, "key_permission_kp");
+#endif
+
+	unregister_kprobe_logged(&input_event_kp, "input_event_kp");
+	unregister_kprobe_logged(&bprm_check_kp, "bprm_check_kp");
+	unregister_kprobe_logged(&vfs_read_kp, "vfs_read_kp");
+}
+
+void unregister_kprobe_thread()
+{
+	unregister_thread = kthread_run(unregister_kprobe_function, NULL, "kprobe_unregister");
+	if (IS_ERR(unregister_thread)) {
+		unregister_thread = NULL;
+		return;
+	}
+}
+
+static void register_kprobe_safer(struct kprobe *kp, const char *name)
+{
+	int ret;
+
+	preempt_disable();
+	ret = register_kprobe(kp);
+	preempt_enable();
+
+	pr_info("register_kprobe: %s ret: %d\n", name, ret);
+
+}
+
+void kp_ksud_init()
+{
+	register_kprobe_safer(&vfs_read_kp, "vfs_read_kp");
+	register_kprobe_safer(&input_event_kp, "input_event_kp");
+	register_kprobe_safer(&bprm_check_kp, "bprm_check_kp");
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
+	register_kprobe_safer(&key_permission_kp, "key_permission_kp");
+#endif
+
+}
